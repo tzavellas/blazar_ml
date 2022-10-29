408804a6d56bd372fa4373d01dcb24f6e8487f7b
dino <dino@MaPet-PRO.gr>
2022-10-29 20:04:25 +0300

    * Add rnn file * Add rnn regressor

---
+++ ml/regressor_rnn.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+import common
+import numpy as np
+import os
+from scipy.stats import reciprocal
+from sklearn.model_selection import RandomizedSearchCV
+import sys
+from tensorflow import keras
+import rnn
+
+
+def regress_rnn():
+    keras_reg = keras.wrappers.scikit_learn.KerasRegressor(rnn.build_model)
+
+    param_distribs = {
+        "n_hidden": [2, 3, 4, 5],
+        "n_neurons": np.arange(10, 2000),
+        "learning_rate": reciprocal(1e-5, 1e-2),
+    }
+
+    rnd_search_cv = RandomizedSearchCV(keras_reg, param_distribs, n_iter=20, cv=3)
+
+    return rnd_search_cv
+
+
+if __name__ == "__main__":
+    np.set_printoptions(precision=4, suppress=True)
+
+    dataset_path = sys.argv[1]
+
+    train_full, test = common.load_data(dataset_path, 0.2) # returns train and test sets
+
+    rnd_search_cv = regress_rnn()
+    # rnd_search_cv = regress_dnn_avg()
+    # rnd_search_cv = regress_dnn_concat()
+
+    rnd_search_cv.fit(*train_full, epochs=150, validation_split=.2,
+                      callbacks=[keras.callbacks.EarlyStopping(patience=10)])
+
+    model = rnd_search_cv.best_estimator_.model
+
+    with open('regress_rnn_report.txt', 'w') as f:
+        f.write('best parameters: {}\n\n'.format(rnd_search_cv.best_params_))
+        f.write('best score: {}\n\n'.format(rnd_search_cv.best_score_))
+        f.write(str(rnd_search_cv.cv_results_))
+
+    if len(sys.argv) > 2:
+        working_dir = sys.argv[2]
+    else:
+        working_dir = os.path.dirname(os.path.realpath(__file__))
+    save_path = os.path.join(working_dir, 'hea_rnn.h5')
+    model.save(save_path)
---
+++ ml/rnn.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+from tensorflow import keras
+
+
+def build_model(n_hidden=2, n_neurons=10, learning_rate=1e-3, input_shape=(6, 1)):
+    model = keras.models.Sequential()
+    model.add(keras.layers.SimpleRNN(n_neurons, input_shape=input_shape, return_sequences=True, dtype=tf.float64))
+    for layer in range(n_hidden):
+        model.add(keras.layers.SimpleRNN(n_neurons, return_sequences=True))
+    model.add(keras.layers.SimpleRNN(n_neurons))
+    model.add(keras.layers.Dense(500))
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(loss=keras.losses.MeanSquaredError(),
+                  metrics=[keras.metrics.RootMeanSquaredError()],
+                  optimizer=optimizer)
+    return model
+
+
+def build_model_lstm(n_hidden=2, n_neurons=10, learning_rate=1e-3, input_shape=(6, 1)):
+    model = keras.models.Sequential()
+    model.add(keras.layers.LSTM(n_neurons, input_shape=input_shape, return_sequences=True, dtype=tf.float64))
+    for layer in range(n_hidden):
+        model.add(keras.layers.LSTM(n_neurons, return_sequences=True))
+    model.add(keras.layers.LSTM(n_neurons))
+    model.add(keras.layers.Dense(500))
+    optimizer = keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(loss=keras.losses.MeanSquaredError(),
+                  metrics=[keras.metrics.RootMeanSquaredError()],
+                  optimizer=optimizer)
+    return model
